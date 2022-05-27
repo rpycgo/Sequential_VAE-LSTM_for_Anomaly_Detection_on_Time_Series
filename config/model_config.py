be016a72cdc0581f32b4709e5fa284f0e5fd78a2
rluver <skyd0608@gmail.com>
2022-05-28 00:27:01 +0900

    Upload model_config

---
+++ config/model_config.py
@@ -0,0 +1,10 @@
+class model_config:
+    encoder = {        
+        'fc2_units': 128,
+        'fc3_units': 64,
+        }
+    decoder = {
+        'time_sequence': 10,
+        'fc2_units': 128,
+        'fc3_units': 256,
+        }
