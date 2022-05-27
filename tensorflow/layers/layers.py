18da147f7f571b80bb79805ba866c0e1c7973860
rluver <skyd0608@gmail.com>
2022-05-28 00:59:17 +0900

    Implements Decoder

--- tensorflow/layers/layers.py
+++ tensorflow/layers/layers.py
@@ -1,7 +1,9 @@
 from ...config import model_config
 
 import tensorflow as tf
-from tensorflow.keras.layers import Layer, Dense, Flatten
+from tensorflow.keras.layers import Layer, Dense, Flatten, Reshape
+
+
 class Sampling(Layer):
     def __init__(self):
         super(Sampling, self).__init__()
@@ -31,4 +33,26 @@ class Encoder(Layer):
         z_log_var = Dense(units=self.latent_dim, name='z_log_var')(x)
         
         return self.sampling(z_mean, z_log_var)
+
+
+class Decoder(Layer):
+    def __init__(self, model_config):
+        super(Decoder, self).__init__()
+        self.model_config = model_config
+
+    def call(self, x, decoder_units1):
+        x = Dense(
+            units=self.model_config.decoder.get('time_sequence') * decoder_units1, 
+            activation='relu', 
+            name='decoder_fc0'
+            )(x)
+        x = Reshape(
+            target_shape=(self.model_config.decoder.get('time_sequence'), self.model_config.decoder.get('fc1_units')), 
+            name='decoder_reshape'
+            )(x)
+        x = Dense(units=self.model_config.decoder.get('fc1_units'), activation="relu", name="decoder_fc1")(x)
+        x = Dense(units=self.model_config.decoder.get('fc2_units'), activation="relu", name="decoder_fc2")(x)
+        x = Dense(units=self.model_config.decoder.get('fc3_units'), activation="relu", name="decoder_fc3")(x)
+
+        return x
 
